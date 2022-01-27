a4c63fca17ac078e962c1a1270568b25fcd0a7e7
rajatmaheshwari2512 <mrajat67@yahoo.com>
2022-01-27 21:42:57 +0530

    changed sample video, and subtitles

--- src/components/Player.js
+++ src/components/Player.js
@@ -110,7 +110,7 @@ const VideoWrap = memo(
             }
         }, [$video]);
 
-        return <video onClick={onClick} src="/sample.mp4?t=1" ref={$video} />;
+        return <video onClick={onClick} src="/sample.mp4" ref={$video} />;
     },
     () => true,
 );

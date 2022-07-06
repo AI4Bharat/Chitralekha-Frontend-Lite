feddfec2064601a3ef40ff808c204b9db3864bd9
payyup <ayushpanwar25@gmail.com>
2022-07-06 22:43:19 +0530

    Add links and user button to a header

--- src/components/Links.js
+++ src/components/Links.js
@@ -4,7 +4,7 @@ export default function Links() {
     return (
         <div className="links" style={{ paddingLeft: '0.3%', paddingTop: '0.5%' }}>
             <a target="_blank" rel="noreferrer" href="https://ai4bharat.org/">
-                <img style={{ width: '100px' }} src="https://i.imgur.com/pVT5Mjp.png" alt="AI4B Logo" />
+                <img style={{ width: '60px' }} src="https://i.imgur.com/pVT5Mjp.png" alt="AI4B Logo" />
             </a>
         </div>
     );

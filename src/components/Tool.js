cecff3ad2cfc28f4813e25e2f359310d1f2bf9e8
A-HK <kahani.0142@gmail.com>
2022-07-08 15:49:16 +0530

    commented out bottom links

--- src/components/Tool.js
+++ src/components/Tool.js
@@ -1146,7 +1146,7 @@ export default function Header({
                     </div>
                 </div>
                 <div className="">
-                    <BottomLinks />
+                    {/* <BottomLinks /> */}
                 </div>
 
                 {/* <div className="translate">

c233e7dfea450e4c3b366bb791ef56daf5d5923f
Gokul NC <gokulnc@ai4bharat.org>
2022-09-01 23:20:29 +0530

    Update Links.js

--- src/components/Links.js
+++ src/components/Links.js
@@ -3,7 +3,7 @@ import React from 'react';
 export default function Links() {
     return (
         <div className="links" style={{ margin: "0 auto 0 10px" }}>
-            <a target="_blank" rel="noreferrer" href="https://ai4bharat.org/">
+            <a target="_blank" rel="noreferrer" href="https://ai4bharat.org/chitralekha">
                 <img style={{ width: '50px' }} src="https://i.imgur.com/pVT5Mjp.png" alt="AI4B Logo" />
             </a>
         </div>

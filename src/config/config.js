78bf40bcc2f0e1491ce8adeba87cf03f6da31b47
sagarika-padmanaban <sagarikapd@gmail.com>
2024-04-26 09:36:11 +0530

    changed base url

--- src/config/config.js
+++ src/config/config.js
@@ -1,5 +1,5 @@
 const configs = {
-    BASE_URL: "https://backend.chitralekha.ai4bharat.org/",
+    BASE_URL: "https://backend.lite.chitralekha.ai4bharat.org/",
     BASE_URL_AUTO: process.env.REACT_APP_APIGW_BASE_URL
       ? process.env.REACT_APP_APIGW_BASE_URL
       : "https://backend.lite.chitralekha.ai4bharat.org"
